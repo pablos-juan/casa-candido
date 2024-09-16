994c91c036f50c85f1389f765553b0af9e54d7f6
pablo <pabloccsanchez@gmail.com>
2024-09-16 14:03:39 -0500

    Menú se oculta automáticamente

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,15 @@
-const hamburger = document.querySelector('.menu-svg');
-const navbarLinks = document.querySelector('.links-list');
+const hamburger = document.querySelector(".menu-svg");
+const navbarLinks = document.querySelector(".links-list");
+const listItems = document.querySelectorAll(".links-list li a");
 
-hamburger.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
-  hamburger.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
 });
